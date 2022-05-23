5ab5fb8e5ee5d4dc5cedee15b709dd3114bfbdd2
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-05-23 13:55:24 +0300

    add transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
+    entry: './src/index',
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist')
+    },
     module: {
         rules: [
             {
@@ -7,11 +14,29 @@ module.exports = {
                 use: {
                     loader: 'ts-loader'
                 }
-            }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
         ]
     },
     resolve: {
-        extensions: [".ts", ".js"],
-
+        extensions: [".ts", ".js"]
+    },
+    devServer: {
+        static: './dist',
+        port: 8080,
+        open: true
     }
 }
